ac0e107b458e8e522a8bdd7241dbebb18d8dd5f2
Peter Henderson <breakend@wpa165234.wireless.mcgill.ca>
2017-02-08 11:04:58 -0500

    More datasets

---
+++ dstc2.py
@@ -0,0 +1,30 @@
+from dstc2_support import dataset_walker
+import sys,os,argparse,shutil,glob,json,copy,time
+import numpy as np
+word_count = 0
+
+turns = []
+
+for dataset in ['dstc2_train', 'dstc2_dev', 'dstc2_test']:
+    # try:
+    sessions = dataset_walker(dataset,dataroot='./data/',labels=True)
+    # except:
+    #     print('Skipping a thing')
+    #     continue
+    start_time = time.time()
+
+    # import pdb; pdb.set_trace()
+
+    for session in sessions:
+        turn_count = 0
+        for turn_index,(log_turn,labels) in enumerate(session):
+            turn_count += 1
+            if 'transcript' in log_turn['output']:
+                word_count += len(log_turn['output']['transcript'].split(' '))
+                # print()
+            if 'transcription' in labels:
+                word_count += len(labels['transcription'].split(' '))
+        turns.append(turn_count)
+
+    print word_count
+    print np.mean(turns)
